110ac157e487fb793b796f4f33ea90bfb1c2f680
m3y <masozawa@pay2.jp>
2020-10-06 00:22:41 +0900

    sandbox

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use kube::{
     api::{Api, ListParams, Meta},
     Client,
 };
+use std::collections::HashSet;
 
 #[derive(Debug)]
 struct PodInfo {
@@ -17,6 +18,31 @@ impl PodInfo {
             name: Meta::name(pod),
             namespace: Meta::namespace(pod).unwrap_or("default".to_string()),
             node_name: pod.spec.as_ref().map(|ps| ps.node_name.clone()).flatten(),
+        }
+    }
+
+    fn contains(&self, target: &String) -> bool {
+        match &self.node_name {
+            Some(node) => node == target,
+            None => false,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct NodeInfo<'a> {
+    name: String,
+    pods: Vec<&'a PodInfo>,
+}
+
+impl NodeInfo<'_> {
+    fn new(name: String, pod_info: &'static Vec<PodInfo>) -> NodeInfo {
+        NodeInfo {
+            name: name.clone(),
+            pods: pod_info
+                .iter()
+                .filter(|p| p.contains(&name))
+                .collect::<Vec<_>>(),
         }
     }
 }
@@ -34,8 +60,21 @@ async fn main() -> anyhow::Result<()> {
         .items
         .iter()
         .map(|p| PodInfo::new(&p))
-        .collect::<Vec<PodInfo>>();
-    println!("{:#?}", &pod_list);
+        .collect::<Vec<_>>();
+
+    let node_names = pod_list
+        .iter()
+        .filter(|p| p.node_name.as_ref().is_some())
+        .map(|p| p.node_name.clone().unwrap())
+        .collect::<HashSet<_>>();
+    println!("{:#?}", &node_names);
+
+    let nodes = node_names
+        .iter()
+        .map(|n| NodeInfo::new(n.to_string(), pod_list.clone()))
+        .collect::<Vec<_>>();
+
+    println!("{:#?}", &nodes);
 
     Ok(())
 }
